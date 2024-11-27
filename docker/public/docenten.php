2e13ea7277e3cdedb06539301a65ba1940992615
vijf7eendrie <rayanaghmer072@outlook.com>
2024-11-27 12:05:13 +0100

    nieuwebestanden

--- docker/public/docenten.php
+++ docker/public/docenten.php
@@ -1,9 +1,15 @@
+<?php
+include_once("../source/database.php");
+
+$connection = database_connect();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title> DHMA Login</title>
+  <title>AGS Login</title>
   <link rel="stylesheet" href="style.css">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   <link rel="stylesheet" href="script.js">
@@ -11,7 +17,7 @@
 <body>
   <div class="wrapper">
     <form action="">
-      <h1>Login Voor Studenten</h1>
+      <h1>Login Voor docenten</h1>
       <div class="input-box">
         <input type="text" placeholder="Gebruikersnaam" required>
         <i class='bx bxs-user'></i>
@@ -20,12 +26,35 @@
         <input type="Wachtwoord" placeholder="Wachtwoord" required>
         <i class='bx bxs-lock-alt' ></i>
       </div>
-      <div class="remember-forgot">
+      <div class="remember-forgot"> 
         <label><input type="checkbox">Onthou me</label>
-        <a href="Docentenlogin.html">Login voor docenten</a>
+        <a href="index.php">Login voor studenten</a>
       </div>
       <button type="submit" class="btn">Login</button>
     </form>
   </div>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- docker/public/index.php
+++ docker/public/index.php
@@ -2,9 +2,39 @@
 include_once("../source/database.php");
 
 $connection = database_connect();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if (!empty($username) && !empty($password)) {
+        $query = "SELECT * FROM users WHERE username = ?";
+        $stmt = $connection->prepare($query);
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+
+            if (password_verify($password, $user['password'])) {
+                session_start();
+                $_SESSION['id'] = $user['id'];
+                $_SESSION['username'] = $user['username'];
+
+                header('Location: dashboard.php');
+                exit();
+            } else {
+                $error_message = "Ongeldige gebruikersnaam of wachtwoord.";
+            }
+        } else {
+            $error_message = "Ongeldige gebruikersnaam of wachtwoord.";
+        }
+    } else {
+        $error_message = "Vul zowel gebruikersnaam als wachtwoord in.";
+    }
+}
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,29 +44,39 @@ $connection = database_connect();
   <title>AGS Login</title>
   <link rel="stylesheet" href="style.css">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-  <link rel="stylesheet" href="script.js">
 </head>
 <body>
   <div class="wrapper">
-    <form action="">
+    <form action="index.php" method="POST">
       <h1>Login Voor Studenten</h1>
+      <?php
+      if (isset($error_message)) {
+          echo "<div class='error'>$error_message</div>";
+      }
+      ?>
       <div class="input-box">
-        <input type="text" placeholder="Gebruikersnaam" required>
+        <input type="text" name="username" placeholder="Gebruikersnaam" required>
         <i class='bx bxs-user'></i>
       </div>
       <div class="input-box">
-        <input type="Wachtwoord" placeholder="Wachtwoord" required>
-        <i class='bx bxs-lock-alt' ></i>
+        <input type="password" name="password" placeholder="Wachtwoord" required>
+        <i class='bx bxs-lock-alt'></i>
       </div>
-      <div class="remember-forgot">
+      <div class="remember-forgot"> 
         <label><input type="checkbox">Onthou me</label>
-        <a href="Docentenlogin.html">Login voor docenten</a>
+        <a href="docenten.php">Login voor docenten</a>
       </div>
       <button type="submit" class="btn">Login</button>
     </form>
   </div>
 </body>
 </html>
+
+
+
+
+
+
 
 
 
